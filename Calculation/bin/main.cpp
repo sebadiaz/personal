e71d39d4ce9baee1c30ad6a989d21e15128f5bd9
Sebastien Diaz <sebastien.diaz@misys.com>
2015-01-29 13:55:32 +0100

    add bin and selectors

--- Calculation/bin/main.cpp
+++ Calculation/bin/main.cpp
@@ -1,47 +1,68 @@
-#include <iostream>
 #include <maxmincounter.h>
 #include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include<list>
+#include <string.h>
 int main(int argc, char **argv)
 {
   std::vector<int> list;
-  list.push_back(5);
-  list.push_back(5);
-  MaxMinCounter max(list);
-  MaxMinCounter max2(list,true);
-  MaxMinCounter max3(list,true,0,2);
-  for(int i=0;i<100;i++){
-    max.read(0,0,0,(rand()%5)+1);
-    max.read(1,0,0,(rand()%5)+1);
-    max2.read(0,0,0,(rand()%5)+1);
-    max2.read(1,0,0,(rand()%5)+1);
-    max3.read(0,rand()%3,0,(rand()%5)+1);
-    max3.read(1,rand()%3,0,(rand()%5)+1);
+  list.push_back(60);
+  list.push_back(60);
+  MaxMinCounter maxii(list);
+  std::ifstream infile;
+  infile.open("euromillions.csv",std::ifstream::in);
+  std::string line;
+  int nbline=0;
+  int startline=1;
+  char buff[1024];
+  while (infile.getline( buff,1024))
+  {
+    line=buff;
+    if(nbline>=startline){
+      std::istringstream iss(line);
+      std::string token;
+      int column=0;
+      char * pch;
+      pch = strtok (line.c_str(),",");
+      while(pch !=NULL)//std::getline(iss, token, ',')&&column<6) {
+	
+	if (column!=0){
+	  std::string token=*pch;
+	  std::istringstream isstok(token);
+	  int a;
+	  if (!(iss >> a)) { 
+	    break; 
+	  } 
+	  else
+	  {
+	    //std::cout << " a = " <<a<<std::endl;
+	    maxii.read(0,nbline,column,a);
+	  }
+	}
+	column++;
+	pch = strtok (NULL, ",");
+      }	
+    }
+    
+    
+    // process pair (a,b)
+    nbline++;
   }
-  max.calculate();
-  max2.calculate();
-  max3.calculate();
-  
-  std::cout <<"PASS : 2 " <<max2.getValues(0,5).size()<< std::endl;
-  std::cout <<"PASS : 2 " <<max2.getValues(0,5).at(0)<<" " <<max2.getScoreValues(0,5).at(0)<<std::endl;
-  std::cout <<"PASS : 2 " <<max2.getValues(0,5).at(1)<<" " <<max2.getScoreValues(0,5).at(1)<<std::endl;
-  std::cout <<"PASS : 2 " <<max2.getValues(0,5).at(2)<<" " <<max2.getScoreValues(0,5).at(2)<<std::endl;
-  std::cout <<"PASS : 2 " <<max2.getValues(0,5).at(3)<<" " <<max2.getScoreValues(0,5).at(3)<<std::endl;
-  std::cout <<"PASS : 2 " <<max2.getValues(0,5).at(4)<<" " <<max2.getScoreValues(0,5).at(4)<<std::endl;
+  std::cout << " a = " <<nbline<<std::endl;
+  infile.close();
+  maxii.calculate();
   
   
-  std::cout <<"PASS :  " <<max.getValues(0,5).size()<< std::endl;
-  std::cout <<"PASS :  " <<max.getValues(0,5).at(0)<<" " <<max.getScoreValues(0,5).at(0)<<std::endl;
-  std::cout <<"PASS :  " <<max.getValues(0,5).at(1)<<" " <<max.getScoreValues(0,5).at(1)<<std::endl;
-  std::cout <<"PASS :  " <<max.getValues(0,5).at(2)<<" " <<max.getScoreValues(0,5).at(2)<<std::endl;
-  std::cout <<"PASS :  " <<max.getValues(0,5).at(3)<<" " <<max.getScoreValues(0,5).at(3)<<std::endl;
-  std::cout <<"PASS :  " <<max.getValues(0,5).at(4)<<" " <<max.getScoreValues(0,5).at(4)<<std::endl;
   
-  std::cout <<"PASS : 3 " <<max3.getValues(0,5).size()<< std::endl;
-  std::cout <<"PASS : 3 " <<max3.getValues(0,5).at(0)<<" " <<max3.getScoreValues(0,5).at(0)<<std::endl;
-  std::cout <<"PASS : 3 " <<max3.getValues(0,5).at(1)<<" " <<max3.getScoreValues(0,5).at(1)<<std::endl;
-  std::cout <<"PASS : 3 " <<max3.getValues(0,5).at(2)<<" " <<max3.getScoreValues(0,5).at(2)<<std::endl;
-  std::cout <<"PASS : 3 " <<max3.getValues(0,5).at(3)<<" " <<max3.getScoreValues(0,5).at(3)<<std::endl;
-  std::cout <<"PASS : 3 " <<max3.getValues(0,5).at(4)<<" " <<max3.getScoreValues(0,5).at(4)<<std::endl;
+  
+  std::cout <<"PASS : 3 " <<maxii.getValues(0,5).size()<< std::endl;
+  std::cout <<"PASS : 3 " <<maxii.getValues(0,5).at(0)<<" " <<maxii.getScoreValues(0,5).at(0)<<std::endl;
+  std::cout <<"PASS : 3 " <<maxii.getValues(0,5).at(1)<<" " <<maxii.getScoreValues(0,5).at(1)<<std::endl;
+  std::cout <<"PASS : 3 " <<maxii.getValues(0,5).at(2)<<" " <<maxii.getScoreValues(0,5).at(2)<<std::endl;
+  std::cout <<"PASS : 3 " <<maxii.getValues(0,5).at(3)<<" " <<maxii.getScoreValues(0,5).at(3)<<std::endl;
+  std::cout <<"PASS : 3 " <<maxii.getValues(0,5).at(4)<<" " <<maxii.getScoreValues(0,5).at(4)<<std::endl;
    
    // std::cout << "Hello, world!" << std::endl;
     std::cout << "PASS : TestCryptoHash::initTestCase()" << std::endl;
